5e0b14afaee2b9fc8e60e340d74c5d9c9d7353e0
Mayank Chugh <mayankchugh1977@gmail.com>
2021-02-20 21:52:05 +0800

    Book Show Page code completed

--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
@@ -11,6 +12,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import PropTypes from 'prop-types';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import BookShow from '../../screens/bookshow/BookShow'
 
 const customStyles = {
     content: {
@@ -132,6 +134,9 @@ class Header extends Component {
         this.setState({contact: e.target.value});
     }
 
+    bookshowHandler = () => {
+        ReactDom.render(<BookShow />, document.getElementById('root'))
+    }
     render(){
         return (
             <div>
@@ -141,7 +146,13 @@ class Header extends Component {
                         <Button variant="contained" color="default" onClick={this.openModalHandler}> 
                             Login 
                         </Button>
-                    </div>                
+                    </div>   
+                    {this.props.showBookShowButton === "true" ?
+                        <div className="bookshow-button">
+                            <Button variant="contained" color="primary" onClick={this.bookshowHandler}>
+                                BOOK SHOW
+                            </Button> 
+                        </div> : "" }     
                 </header>
                 <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" 
                     onRequestClose={this.closeModalHandler}  style={customStyles}> 
